0c91f624ababf40a00d11691ffa0686e1f003ae7
Olga Zbrueva <oezbrueva@edu.hse.ru>
2026-04-14 23:07:13 +0500

    Создание шаблона класса SettingsService

--- ClassLibrary/SettingsService.cs
+++ ClassLibrary/SettingsService.cs
@@ -8,5 +8,77 @@ namespace SmartBudget.ClassLibrary
 {
     internal class SettingsService
     {
+        public string _language;
+        public bool _isDark;
+        public bool _isLeftHanded;
+        public bool _isDogTheme;
+        // РЕШИТЬ С ВАЛЮТОЙ
+
+        public string Language
+        {
+            get { return _language; }
+            set
+            {
+                _language = value;
+            }
+        }
+
+        public bool IsDark
+        {
+            get { return _isDark; }
+            set
+            {
+                _isDark = value;
+            }
+        }
+
+        public bool IsLeftHanded
+        {
+            get { return _isLeftHanded; }
+            set
+            {
+                _isLeftHanded = value;
+            }
+        }
+        public bool IsDogTheme
+        {
+            get { return _isDogTheme; }
+            set
+            {
+                _isDogTheme = value;
+            }
+        }
+
+        public SettingsService(string language, bool isDark, bool isLeftHanded, bool isDogTheme)
+        {
+            Language = language;
+            IsDark = isDark;
+            IsLeftHanded = isLeftHanded;
+            IsDogTheme = isDogTheme;            
+        }
+
+        public SettingsService()
+        {
+            Language = "Русский";
+            IsDark = false;
+            IsLeftHanded = false;
+            IsDogTheme = false;
+        }
+
+        public SettingsService LoadSettings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveSettings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public SettingsService ResetSettigs()
+        {
+            throw new NotImplementedException();
+        }
+               
     }
 }
