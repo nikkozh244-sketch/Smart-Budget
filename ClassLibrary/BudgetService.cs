ac09690484a4a4702c3e20189acb613330c1bd4d
Nikita Kozhevnikov <nikalekozhevnikov@edu.hse.ru>
2026-04-14 22:13:40 +0500

    Теперь классы в библиотеке

--- ClassLibrary/BudgetService.cs
+++ ClassLibrary/BudgetService.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SmartBudget
+namespace SmartBudget.ClassLibrary
 {
-    internal class ObjectOfAnalysis
+    internal class BudgetService
     {
     }
 }
