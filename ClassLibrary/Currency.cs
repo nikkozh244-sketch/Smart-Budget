529559513dec89c98d37648ca090348c8dfe8519
Nikita Kozhevnikov <nikalekozhevnikov@edu.hse.ru>
2026-04-14 23:45:50 +0500

    Введен шаблон класса Currence для определения размера операции

---
+++ ClassLibrary/Currency.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartBudget.ClassLibrary
+{
+    internal class Currency
+    {
+        public float _amount;
+
+        public float Amount
+        { 
+            get { return _amount; } 
+            set 
+            {
+                string tempAmount = _amount.ToString();
+                int indexOfComa = tempAmount.IndexOf(",");
+                int amountLength = tempAmount.Length;
+
+                if (amountLength - indexOfComa > 2)
+                    throw new Exception("Ошибка! Вводите сумму операции с не более чем двумя знаками после запятой!");
+                _amount = value; 
+            } 
+        }
+
+        public void Init()
+        {
+            if (float.TryParse(Console.ReadLine(), out float tempAmount))
+                Amount = tempAmount;
+            else
+                throw new Exception("Ошибка! Значение введено некорректно");
+        }
+
+        public Currency()
+        {
+            Amount = 0;
+        }
+
+        public Currency(float amount)
+        {
+            Amount = amount;
+        }
+
+        public virtual string ToString()
+        {
+            return $"{_amount}";
+        }
+    }
+}
